fb0f8ca8936180260baad9e294625575dda3b619
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-16 18:55:51 +0800

    completed local storage

---
+++ assets/localstore.js
@@ -0,0 +1,35 @@
+var nineTask = $("#input1");
+    var nineText =localStorage.getItem("09");
+    nineTask.val(nineText);
+
+    var tenTask = $("#input2");
+    var tenText =localStorage.getItem("10");
+    tenTask.val(tenText);
+
+    var elevenTask = $("#input3");
+    var elevenText =localStorage.getItem("11");
+    elevenTask.val(elevenText);
+
+    var twelveTask = $("#input4");
+    var twelveText =localStorage.getItem("12");
+    twelveTask.val(twelveText);
+
+    var oneTask = $("#input5");
+    var oneText =localStorage.getItem("13");
+    oneTask.val(oneText);
+
+    var twoTask = $("#input6");
+    var twoText =localStorage.getItem("14");
+    twoTask.val(twoText);
+
+    var threeTask = $("#input7");
+    var threeText =localStorage.getItem("15");
+    threeTask.val(threeText);
+
+    var fourTask = $("#input8");
+    var fourText =localStorage.getItem("16");
+    fourTask.val(fourText);
+
+    var fiveTask = $("#input3");
+    var fiveText =localStorage.getItem("17");
+    fiveTask.val(fiveText);
--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,98 @@
 
-function onReady(){
-    
+function onReady() {
+
     const timeElement = $("#currentDay");
     const currentDay = moment().format("dddd MMMM Do");
-    const currentTime = moment().format("HH:mm");
+    let currentTime = moment().format("HH");
+    currentTime = Number(currentTime);
+
     //create a time/day tracker that starts when the page loads 
-    function setTime(){
-    timeElement.text(currentDay);
-    //insert date into the header
-    console.log(currentTime);
-   }
-   
-    setInterval(function (){
-       setTime();
-       nineColor();
-       tenColor();
-       elevenColor();
-       twelveColor();
-       oneColor();
-       twoColor();
-       threeColor();
-       fourColor();
-       fiveColor();
-    },1000);
+    function setTime() {
+        timeElement.text(currentDay);
+        //insert date into the header
+        //console.log(currentTime);
+    }
 
-    //save text content from input into local storage
-    //click btn
-    //save text input to local storage
+    var nineTask = $("#input1");
+    var nineText =localStorage.getItem("09");
+    console.log(nineTask);
+    nineTask.val(nineText);
+
     
-    // clear input
+    var nineTask = $("#input1");
+    var nineText =localStorage.getItem("09");
+    console.log(nineTask);
+    nineTask.val(nineText);
+
+    function colourChange() {
+        // target all the row 
+        const rows = $(".task");
+        //console.log(rows);
+
+        // loop thru these rows
+        rows.each(function (i, row) {
+            //console.log(i); // i is the index of the element
+            //console.log(row); // row element
+            // for each row we will run the time check
+
+            // grab the data-hour attr
+            var currentTask = $(row).data("hour");
+            currentTask = Number(currentTask);
+            // if row time is less than current time
+            // grey
+            // if row time is === current time
+            // red
+
+            // if row time is > current 
+            // green
+            if (currentTask == currentTime) {
+                $(row).addClass("current");
+                $(row).removeClass("past");
+                $(row).removeClass("future");
+            }
+            if (currentTask < currentTime) {
+                $(row).addClass("past");
+                $(row).removeClass("current");
+                $(row).removeClass("future");
+            }
+            if (currentTask > currentTime) {
+                $(row).addClass("future");
+                $(row).removeClass("past");
+                $(row).removeClass("current");
+            }
+        });
+
+    }
+    setInterval(function () {
+        setTime();
+        colourChange();
+
+    }, 1000);
+
+    const savebtn = $(".save");
+
+    savebtn.on("click", function (event) {
+
+        const jButton = $(event.target);
+        const jButtonCol = jButton.parents();
+        const jButtonUserInput = jButtonCol.prev().children();
+        const textInput = jButtonUserInput.val();
+        const timeOfTask = jButtonCol.prev(".task");
+        const timeOfTaskVal = timeOfTask.data("hour");
+     
+
+        localStorage.setItem(timeOfTaskVal, textInput);
+        console.log(localStorage);
+     
+
+    });
 
 
+    //get value from input
+
+    //save it with get
+
+    //set text content of task
 
 }
 $(onReady);
-
-
-
-
-
-
-
-
-
-
-//make a function for current hour
-//change the colour to red/current
-
-//function for if an hour  has past
-//change class to grey/past
-
-//create function for future
-//change class to future/green
-
-//function for entering data in table
-
-//fucntion for saving to local storage
-//click event to tagret the current row and store the data
-
-//fucntion for clearing contence
